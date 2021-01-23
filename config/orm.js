3191096cbdad0e125ee7e2b0540810d1de1fdd2d
Jorge Martinez <70128888+jamjorge21@users.noreply.github.com>
2021-01-23 08:54:56 -0800

    Update orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,85 +1,28 @@
 var connection = require("../config/connection");
 
-function createQmarks(num) {
-  var arr = [];
-  for (var i = 0; i < num; i++) {
-    arr.push("?");
-  }
-  return arr.toString();
-}
-
-function translateSql(ob) {
-  var arr = [];
-  for (var key in ob) {
-    var value = ob[key];
-    if (Object.hasOwnProperty.call(ob, key)) {
-      if (typeof value === "string" && value.indexOf(" ") >= 0) {
-        value = "'" + value + "'";
-      }
-      arr.push(key + "=" + value);
-    }
-  }
-  return arr.toString();
-}
-
 var orm = {
-  selectAll: function(table, cb) {
-    var dbQuery = "SELECT * FROM " + table + ";";
+    selectAll: function(tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
 
-    connection.query(dbQuery, function(err, res) {
-      if (err) {
-        throw err;
-      }
-      cb(res);
-    });
-  },
-  insertOne: function(table, cols, vals, cb) {
-    var dbQuery =
-      "INSERT INTO " +
-      table +
-      " (" +
-      cols.toString() +
-      ") " +
-      "VALUES (" +
-      createQmarks(vals.length) +
-      ") ";
-
-    console.log(dbQuery);
-    connection.query(dbQuery, vals, function(err, res) {
-      if (err) {
-        throw err;
-      }
-      cb(res);
-    });
-  },
-  updateOne: function(table, objColVals, condition, cb) {
-    var dbQuery =
-      "UPDATE " +
-      table +
-      " SET " +
-      translateSql(objColVals) +
-      " WHERE " +
-      condition;
-
-    console.log(dbQuery);
-
-    connection.query(dbQuery, function(err, res) {
-      if (err) {
-        throw err;
-      }
-      cb(res);
-    });
-  },
-  deleteOne: function(table, condition, cb) {
-    var dbQuery = "DELETE FROM " + table + " WHERE " + condition;
-    console.log(dbQuery);
-
-    connection.query(dbQuery, function(err, res) {
-      if (err) {
-        throw err;
-      }
-      cb(res);
-    });
-  }
+        connection.query(queryString, function(err, res) {
+            if (err) throw err;
+            cb(res);
+        });
+    },
+    insertOne: function(tableInput, val, cb) {
+        connection.query("INSERT INTO " + tableInput + " (burger_name) VALUES ('" + val + "');",
+        function(err, result) {
+            if (err) throw err;
+            cb(result);
+        })
+    },
+    updateOne: function(tableInput, condition, cb) {
+        connection.query("UPDATE " + tableInput + " SET devoured=true WHERE id=" + condition + ";", function(err, res) {
+            if (err) throw err;
+            cb(res);
+        })
+    }
 };
+
+
 module.exports = orm;
