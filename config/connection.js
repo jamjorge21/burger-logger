9584c0cbc76cb437ebb4b873fd5a123c2b796a06
Jorge Martinez <70128888+jamjorge21@users.noreply.github.com>
2021-01-23 08:53:53 -0800

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,23 @@
-const mysql = require("mysql");
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
+var mysql = require ("mysql");
+
+var connection = mysql.createConnection({
     host: "localhost",
+    port: 3306,
     user: "root",
     password: "Bru39025no!",
     database: "burgers_db"
   });
-}
-connection.connect();
+  
+  // Make connection.
+  connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+  
+  // Export connection for our ORM to use.
+  module.exports = connection;
+
 module.exports = connection;
